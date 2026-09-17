d049b5b720bfab136cb8f1bb9ea997d6b421fc33
Dinamitrii <k_karaivanov@abv.bg>
2026-09-17 17:59:18 +0300

    variant euro store img change on category db migration color changes logo added sitemap and robots.txt routes and files

--- app.py
+++ app.py
@@ -769,6 +769,22 @@ def init_db_command():
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
+@app.route("/favicon.ico")
+def favicon():
+    return (url_for('static', filename='img/favicon/favicon.ico'),
+            url_for('static', filename='img/favicon/favicon-16x16.png'),
+            url_for('static', filename='img/favicon/favicon-32x32.png'),
+            url_for('static', filename='img/favicon/android-chrome-192x192.png'),
+            url_for('static', filename='img/favicon/android-chrome-256x256.png'),
+            url_for('static', filename='img/favicon/apple-touch-icon.png'),
+            url_for('static', filename='img/favicon/safari-pinned-tab.svg'),
+            url_for('static', filename='img/favicon/mstile-150x150.png'),
+            url_for('static', filename='img/favicon/browserconfig.xml'),
+            url_for('static', filename='img/favicon/site.webmanifest'))
+
+
+
+
 
 if __name__ == '__main__':
     with app.app_context():
